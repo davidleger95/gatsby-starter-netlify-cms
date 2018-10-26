1bffbdef49ed602b2126d4e6debd290a42c4b241
David Leger <davidleger95@me.com>
2018-10-26 17:27:10 -0300

    add SSR support for styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,12 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
